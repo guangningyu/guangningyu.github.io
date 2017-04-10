7d9febd6646656f300e38256441e7ce4c26d4b27
Guangning Yu <hi@guangningyu.com>
2017-04-10 23:41:01 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 $(document).ready(function() {
     $('.btn-mobile-menu__icon').click(function() {
         if ($('.navigation-wrapper').css('display') == "block") {
-            // $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            console.log('> bounceOutUp');
+            $('.navigation-wrapper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
               $('.navigation-wrapper').toggleClass('visible animated bounceOutUp');
-            // });
-            // $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
+            });
+            $('.navigation-wrapper').toggleClass('animated bounceInDown animated bounceOutUp');
         } else {
+            console.log('> bounceInDown');
             $('.navigation-wrapper').toggleClass('visible animated bounceInDown');
         }
+        console.log('> ToggleIcon');
         $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
     });
 });
